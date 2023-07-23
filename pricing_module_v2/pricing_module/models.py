24be59eccc6a59eac7cfcff95b00a47ec675cba2
Sunil1208 <skmy03@gmail.com>
2023-07-23 21:08:59 +0530

    added is_active field

--- pricing_module_v2/pricing_module/models.py
+++ pricing_module_v2/pricing_module/models.py
@@ -6,6 +6,7 @@ class PricingConfig(models.Model):
     distance_additional_price = models.DecimalField(max_digits=8, decimal_places=2)
     time_multiplier_factor = models.DecimalField(max_digits=8, decimal_places=2)
     waiting_charges = models.DecimalField(max_digits=8, decimal_places=2)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
