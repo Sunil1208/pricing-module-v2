fb90768057e1fa7a298dfe29c88e11b6af42d990
Sunil1208 <skmy03@gmail.com>
2023-07-23 15:10:45 +0530

    added pricing_modue in installed apps and added view for pricing api

--- pricing_module_v2/pricing_module/views.py
+++ pricing_module_v2/pricing_module/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import PricingConfig
 
-# Create your views here.
+def calculate_pricing(request, additional_distance, ride_time, waiting_time):
+    try:
+        # Fetch the active pricing configuration (you can define the logic for this)
+        active_config = PricingConfig.objects.filter(is_active=True).latest('created_at')
+        
+        # Perform the pricing calculation using the provided formula
+        price = (active_config.distance_base_price + (additional_distance * active_config.distance_additional_price)) \
+                + (ride_time * active_config.time_multiplier_factor) \
+                + (waiting_time * active_config.waiting_charges)
+        
+        return JsonResponse({'price': price})
+    except PricingConfig.DoesNotExist:
+        return JsonResponse({'error': 'No active pricing configuration found.'}, status=404)
+
