a011606953e8fea385d0d3f5c6d2beac2908995e
Sunil Kumar <skmy03@gmail.com>
2023-07-23 14:24:13 +0530

    project init and added model for price config

---
+++ pricing_module_v2/pricing_module/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import PricingConfig
+
+admin.site.register(PricingConfig)
---
+++ pricing_module_v2/pricing_module/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class PricingConfig(models.Model):
+    name = models.CharField(max_length=100)
+    distance_base_price = models.DecimalField(max_digits=8, decimal_places=2)
+    distance_additional_price = models.DecimalField(max_digits=8, decimal_places=2)
+    time_multiplier_factor = models.DecimalField(max_digits=8, decimal_places=2)
+    waiting_charges = models.DecimalField(max_digits=8, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
